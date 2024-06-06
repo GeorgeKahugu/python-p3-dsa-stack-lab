7cb24bc80a39c77f452e0de2744f70e413f7738c
GeorgeKahugu <gee.kahugu@gmail.com>
2024-06-06 20:02:49 +0300

    dsa stack

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,36 @@
-class Stack:
 
-    def __init__(self, items = [], limit = 100):
-        pass
+class Stack:
+    def __init__(self, items=None, limit=100):
+        self.items = items if items is not None else []
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        return len(self.items) == 0
 
     def push(self, item):
-        pass
+        if len(self.items) < self.limit:
+            self.items.append(item)
+        else:
+            raise OverflowError("Stack limit reached")
 
     def pop(self):
-        pass
+        if self.isEmpty():
+            raise IndexError("pop from an empty stack")
+        return self.items.pop()
 
     def peek(self):
-        pass
-    
+        if self.isEmpty():
+            raise IndexError("peek from an empty stack")
+        return self.items[-1]
+
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        return len(self.items) == self.limit
 
     def search(self, target):
-        pass
+        try:
+            return self.items[::-1].index(target)
+        except ValueError:
+            return -1
--- lib/testing/stack_test.py
+++ lib/testing/stack_test.py
@@ -1,3 +1,4 @@
+
 from Stack import Stack
 
 class TestStack:
@@ -37,22 +38,26 @@ class TestStack:
         stk = Stack()
         assert(stk.isEmpty())
         assert(stk.size() == 0)
-        assert(stk.pop() == None)
+        try:
+            stk.pop()
+        except IndexError as e:
+            assert(str(e) == "pop from an empty stack")
         stk.push(1)
         assert(not stk.isEmpty())
         assert(stk.size() == 1)
         assert(stk.pop() == 1)
 
-
     def test_full(self):
         '''Test Stack full() method'''
         stk = Stack([1], 1)
-
         assert(stk.full())
         assert(stk.size() == 1)
         assert(stk.pop() == 1)
         stk.push(1)
-        stk.push(2)
+        try:
+            stk.push(2)
+        except OverflowError as e:
+            assert(str(e) == "Stack limit reached")
         assert(stk.full())
         assert(stk.size() == 1)
         assert(stk.pop() == 1)
@@ -60,13 +65,11 @@ class TestStack:
     def test_search(self):
         '''Test Stack search() method. How far is the element in the stack? '''
         stk = Stack([5,6,7,8,9,10])
-
         assert(stk.search(5) == 5)
         assert(stk.search(6) == 4)
         assert(stk.search(7) == 3)
         assert(stk.search(8) == 2)
         assert(stk.search(9) == 1)
         assert(stk.search(10) == 0)
-
         # Case with target not in Stack
         assert(stk.search(15) == -1)
